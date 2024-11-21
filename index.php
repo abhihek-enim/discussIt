cba38c864d1163dddbc95f8afd50222064d1e4df
abhishek <shukla13122000@gmail.com>
2024-11-21 20:00:33 +0530

    initialized project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>Discuss Project</title>
+     <?php include('./client/commonFiles.php') ?>
+</head>
+
+<body>
+    
+<?php include('./client/header.php') ?>
+</body>
+</html>
